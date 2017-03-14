854026570fbd1377c39e5cc5e51a7358d230eefa
cloudera <cloudera@quickstart.cloudera>
2017-03-14 03:08:25 -0700

    third changes

---
+++ jupyterwf/data.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+from urllib.request import urlretrieve
+
+FREMONT_URL='https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv',url=FREMONT_URL,force_download=False):
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url,filename)
+    data=pd.read_csv('Fremont.csv',index_col='Date',parse_dates=True) 
+    data.columns=['West','East']
+    data['Total'] = data['West'] + data['East']
+    
+    return data
